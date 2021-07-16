56fa6545a4d321f32dcc84a624f79772a64496b0
Grant Schumacher <grantschu99@gmail.com>
2021-07-16 12:48:30 -0600

    temporarily removed AOS because of loading issues. Cleaned up css

--- js/aos-listeners.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('aos:in:grant', ({ detail }) => {
-    console.log("in")
-
-  });
-  
-  document.addEventListener('aos:out:grant', ({ detail }) => {
-    console.log("out")
-  });
